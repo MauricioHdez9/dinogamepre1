1f97a6cf0ca15b2e751e889edd336e8c0f0b1b12
tuxryu <shiwarrokur@gmail.com>
2022-09-23 10:33:56 -0500

    agregando script en back

---
+++ scripts.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+//---- boton de pausa ---------------------------------------------------------------------------------------- 
+function stopanimation()
+{
+    cactus.style.animationPlayState = 'paused'
+    player.style.animationPlayState = 'paused'
+    background.style.animationPlayState ='paused'
+     
+
+}
+function stopscore()
+{
+    clearInterval(scoreintervalId)
+    clearInterval(scoreintervalId)
+
+}
+function resumencore()
+{
+    scoreintervalId = setInterval(()=>
+    {
+        score++
+        document.getElementById("score").innerText = score
+
+    },1000)
+
+
+}
+function pausegame()
+{
+    stopscore();
+    stopanimation();
+    stopscore();
+    
+    
+     
+     
+}
+function resumengame()
+{
+    resumencore();
+     cactus.style.animationPlayState = 'running'
+     player.style.animationPlayState = 'running'
+     background.style.animationPlayState ='running'
+    //  resumencore();
+     
+}
+
+
+buttonPlayStop.addEventListener('click', () => {
+    if (buttonPlayStop.classList.contains("play")) {
+        resumengame();
+    } 
+    else {
+        
+        pausegame();
+    }
+    buttonPlayStop.classList.toggle("play");
+})
