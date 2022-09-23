99c13842aede3de9ae1d0cf7f72483ba0e735993
luk <luisk5162@gmail.com>
2022-09-23 10:40:50 -0500

    se agrga codigo en js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+/*primer de commit luis en js*/ 
+const player = document.getElementById("player");
+
+document.addEventListener("click",function () {
+    player.classList.add("playerJump");
+})  
 
 
 
