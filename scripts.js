82d99c4404f0bff9a9f5694bf83f84a2892403ac
luk <luisk5162@gmail.com>
2022-09-23 10:49:17 -0500

    comint de lalinea 6 en adelante es js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,10 @@ document.addEventListener("click",function () {
     player.classList.add("playerJump");
 })  
 
-
-
+player.addEventListener('animationend',() => {
+    player.classList.remove("playerJump");
+});
+/*segundo commit en js */
 
 
 
